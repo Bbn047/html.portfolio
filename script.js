22c91cf03b26e0d75b4355498f42fbd0ecd77864
Bibin Thomas <bibinthomas4897@gmail.com>
2025-03-27 00:46:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    menu.classList.remove('active')
+}
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Junior Developer.', 'Web Designer.','Embedded Deleloper.', 'Artist.'],
+    typeSpeed: 50,
+    backSpeed:80,
+    backDelay: 1200,
+    loop: true,
+  });
